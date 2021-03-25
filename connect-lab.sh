796cfe29cf378c3dce792ec6c98a298c8257c368
vitome @ kathara <angelo.spognardi@gmail.com>
2021-03-25 11:56:07 +0100

    Fixed connect-lab: get network name

--- connect-lab.sh
+++ connect-lab.sh
@@ -5,11 +5,17 @@ sudo ip link del veth1 type veth
 sudo ip link add veth1 type veth
 sudo ip addr flush veth0
 sudo ip addr add $1 dev veth0
-bridge="$(brctl show | grep -o 'kt-[a-z0-9]*')"
+if [ -z "$2" ]
+then
+   echo "Network name not provided: assume only one lan exists..."
+   bridge="$(docker network ls | grep -o '^[a-z0-9]*')"
+else
+   bridge="$(docker network ls | grep $2 | grep -o '^[a-z0-9]*')"
+fi
 
-echo "\n attaching veth1 to bridge: ${bridge}"
+echo "\n attaching veth1 to bridge: kt-${bridge}"
 
-sudo ip link set veth1 master ${bridge}
+sudo ip link set veth1 master kt-${bridge}
 
 sudo ip link set veth0 up
 sudo ip link set veth1 up
