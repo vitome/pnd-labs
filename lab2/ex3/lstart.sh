9236b9506d1c49c9e795ef7989fcafe23a8296f9
vitome @ kathara <angelo.spognardi@gmail.com>
2022-03-24 12:14:18 +0100

    lstart

--- lab2/ex3/lstart.sh
+++ lab2/ex3/lstart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-xrdb ~/.XDefaults
+xrdb ../.XDefaults
 sudo kathara lstart --privileged
 for p in pc1 pc2 
 do
   xterm -e bash -c "kathara connect $p" &
 done
-xrdb ~/.XDefaults.alt
+xrdb ../.XDefaults.alt
 xterm -e bash -c "kathara connect r1" &
 
